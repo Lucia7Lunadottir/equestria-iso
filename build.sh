672437ad4b8ba752e7b851f5442595f7a8ffa6cb
Bordiian Georgii <bordiyan20035@gmail.com>
2026-03-13 02:25:00 +0400

    Keyring fix

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -t 0 ]; then
+    konsole -e bash "$0" "$@"
+    exit 0
+fi
+
+
+for m in $(tac /proc/mounts | grep "equestria-iso/work" | awk '{print $2}'); do
+    sudo umount -f -l "$m"
+done
+
+sudo rm -rf ~/equestria-iso/work
+
+sudo mkarchiso -v -o ~/equestria-out .
